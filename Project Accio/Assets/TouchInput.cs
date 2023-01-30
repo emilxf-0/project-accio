e6b926744f7a7ea65a3c87b93c1b451cf5a8959b
Emil Forsén <emilxforsen@gmail.com>
2023-01-30 12:54:22 +0100

    We can now see the line get drawn as it gets drawn, and then it fades.

--- Project Accio/Assets/TouchInput.cs
+++ Project Accio/Assets/TouchInput.cs
@@ -1,25 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 public class TouchInput : MonoBehaviour
 {
     private List<Vector3> userInputPoints = new List<Vector3>();
 
-    LineRenderer lineRenderer;
+    private LineRenderer lineRenderer;
     private Timer timer;
 
     private float fadeTime = 1f;
     private float fadeStartTime;
+    private bool startFade;
     
     void Start()
     {
-        lineRenderer = gameObject.AddComponent<LineRenderer>();
-        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
-        lineRenderer.startColor = Color.white;
-        lineRenderer.endColor = Color.white;
-        lineRenderer.startWidth = 0.1f;
-        lineRenderer.endWidth = 0.1f;
+        CreateNewLineRenderer();
     }
     
     void Update()
@@ -27,7 +24,7 @@ public class TouchInput : MonoBehaviour
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
-            
+
             if (touch.phase == TouchPhase.Moved)
             {
                 Vector3 screenPos = touch.position;
@@ -41,9 +38,23 @@ public class TouchInput : MonoBehaviour
             if (touch.phase == TouchPhase.Ended)
             {
                 fadeStartTime = Time.time;
+                startFade = true;
             }
+
         }
 
+        if (startFade == false)
+        {
+            return;
+        }
+        
+        FadeLine();
+       
+        
+    }
+    
+    void FadeLine()
+    {
         float timeSinceStart = Time.time - fadeStartTime;
         
         if (timeSinceStart < fadeTime)
@@ -54,23 +65,16 @@ public class TouchInput : MonoBehaviour
             lineRenderer.endColor = Color.Lerp(Color.white, Color.clear, t);
         }
 
-        if (Input.GetKeyDown(KeyCode.C))
-        {
-            lineRenderer.positionCount = 0;
-        }
-        
     }
 
-    void ClearLines()
+    void CreateNewLineRenderer()
     {
-        userInputPoints.Clear();
-        lineRenderer.SetPositions(userInputPoints.ToArray());
-    }
-
-    void FadeLine(float fadeStart)
-    {
-        lineRenderer.startColor = Color.Lerp(Color.white, Color.clear, 0);
-        lineRenderer.endColor = Color.Lerp(Color.white, Color.clear, 0);
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+        lineRenderer.material = new Material(Shader.Find("Sprites/Default"));
+        lineRenderer.startColor = Color.white;
+        lineRenderer.endColor = Color.white;
+        lineRenderer.startWidth = 0.1f;
+        lineRenderer.endWidth = 0.1f;
     }
 
 }
